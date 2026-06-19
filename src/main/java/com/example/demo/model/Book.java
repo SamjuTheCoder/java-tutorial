ea1603c31a5471a6fa668faeb90f2b671f79a12a
Julius Fasema <samjusoftware.com@gmail.com>
2026-06-19 22:01:15 +0100

    added book service

--- src/main/java/com/example/demo/model/Book.java
+++ src/main/java/com/example/demo/model/Book.java
@@ -1,4 +1,45 @@
 package com.example.demo.model;
 
 public class Book {
+
+    // define properties
+    private Long id;
+    private String title;
+    private String author;
+
+    // define no arg constructor
+    public Book(){}
+
+    // define arg constructor
+    public Book( Long id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+
+    // getter and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 }
