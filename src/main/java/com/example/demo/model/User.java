d52f4b2e062bc9d5e747991edaeb355cb048c6fe
unknown <akntuen@yahoo.com>
2026-07-01 10:13:50 +0100

    user entity table mapping

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,20 +1,32 @@
 package com.example.demo.model;
 
 import com.example.demo.enums.Gender;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.engine.jdbc.Size;
 
-import java.util.Enumeration;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+
+@Entity
+@Table(name = "tbl_users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(length = 45, nullable = false, unique = false)
     private String name;
+    @Column(length = 100, nullable = false, unique = true)
     private String email;
+    @Enumerated(EnumType.STRING)
+    @Column(length = 10, nullable = false)
     private Gender gender;
+    @Column(length = 14, nullable = false, unique = true)
     private String phone;
 }
